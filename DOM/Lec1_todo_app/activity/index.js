bc690af7d2ce5f44d2cb5e95a364a635ffdb9322
Jatin-Goyal5 <jatinji145@gmail.com>
2021-04-16 20:30:52 +0530

    js functionality added

--- DOM/Lec1_todo_app/activity/index.js
+++ DOM/Lec1_todo_app/activity/index.js
@@ -4,29 +4,46 @@ let todolist = document.querySelector('.todo-list');
 
 
 todobutton.addEventListener("click", function(data){
+    addTodo();
+   
+})
+
+
+
+todoinput.addEventListener("keypress" , function(event){
+    if(event.key == 'Enter'){
+        addTodo();
+    }
+    
+})
+
+function addTodo(){
     let todo = todoinput.value;
     if(todo){
+        // create list element
         let listitem = document.createElement('li');
         listitem.classList.add('todo-item');
 
+        // create pTag
         let pTag = document.createElement('p');
         pTag.classList.add('todo');
         pTag.innerHTML = todo;
 
+        // create deleteButtton
         let deleteButtton = document.createElement('button');
         deleteButtton.classList.add('Delete-todo');
-        deleteButtton.innerHTML = 'DELETE TASK'
+        deleteButtton.innerHTML = 'DELETE TASK';
+        
+        deleteButtton.addEventListener('click', function(event){
+          event.target.parentNode.remove();  
+        })
 
+        // add todo to list Element
         listitem.append(pTag);
         listitem.append(deleteButtton)
         todolist.append(listitem);
 
-    }
-})
+        todoinput.value =" ";
 
-// todoinput.addEventListener("keypress" , function(event){
-//     if(event.key == 'Enter'){
-//         console.log(event.key)
-//     }
-    
-// })
+    }
+}
